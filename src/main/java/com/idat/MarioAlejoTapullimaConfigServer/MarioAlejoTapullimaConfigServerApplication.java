e0dc12a529fbfe8ffd60e195012e9cfce7390778
24Alan <86678009+24Alan@users.noreply.github.com>
2022-06-03 23:55:57 -0500

    Add files via upload

---
+++ src/main/java/com/idat/MarioAlejoTapullimaConfigServer/MarioAlejoTapullimaConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.idat.MarioAlejoTapullimaConfigServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarioAlejoTapullimaConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarioAlejoTapullimaConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/idat/MarioAlejoTapullimaConfigServer/MarioAlejoTapullimaConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idat.MarioAlejoTapullimaConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarioAlejoTapullimaConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
